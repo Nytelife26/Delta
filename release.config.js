6c0f14d9bc92488aa670bc25114fc46f2b689226
Nytelife26 <xtylerjrx@gmail.com>
2020-11-05 11:27:09 +0000

    chore(deps): prepare workflow and linting

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		[
+			"@semantic-release/npm",
+			{
+				npmPublish: true
+			}
+		],
+		"@semantic-release/github",
+		"@semantic-release/git"
+	]
+}
